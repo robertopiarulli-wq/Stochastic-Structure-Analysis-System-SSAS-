dac604a87b222e91bb1baf02846b9b84e5fdd8d5
robertopiarulli-wq <roberto.piarulli@gmail.com>
2026-05-01 17:17:12 +0200

    Delete core directory

--- core/generator.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def generate_sestina():
-    return sorted(random.sample(range(1, 91), 6))
-
-
-def generate_ensemble(n=100000):
-    for _ in range(n):
-        yield generate_sestina()
--- core/scoring.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def score(h, target_h, delta, target_delta, w_h=1, w_delta=10):
-    err_h = abs(h - target_h)
-    err_delta = abs(delta - target_delta)
-    
-    return (w_h * err_h) + (w_delta * err_delta)
