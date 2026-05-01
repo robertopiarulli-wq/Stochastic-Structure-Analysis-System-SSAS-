6e5c2fd691d5a477924eded69af6761506f55f54
robertopiarulli-wq <roberto.piarulli@gmail.com>
2026-05-01 17:17:45 +0200

    Delete engine directory

--- engine/run_simulation.py
+++
@@ -1,9 +0,0 @@
-from core.generator import generate_ensemble
-from core.features import features
-
-def run_simulation(n=100000):
-    results = []
-    for s in generate_ensemble(n):
-        f = features(s)
-        results.append((s, f["H"]))
-    return results
