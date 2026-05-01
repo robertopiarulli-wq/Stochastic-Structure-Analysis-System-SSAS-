9439a742988b09204f2d4bb815bfd1bc41a089bb
robertopiarulli-wq <roberto.piarulli@gmail.com>
2026-05-01 15:56:05 +0200

    Update scoring.py

--- core/scoring.py
+++ core/scoring.py
@@ -1,4 +1,7 @@
 import numpy as np
 
-def score(h, target_h, delta, target_delta):
-    return abs(h - target_h) + 10 * abs(delta - target_delta)
+def score(h, target_h, delta, target_delta, w_h=1, w_delta=10):
+    err_h = abs(h - target_h)
+    err_delta = abs(delta - target_delta)
+    
+    return (w_h * err_h) + (w_delta * err_delta)
