d0f87a5059bd8047a36405450cdb534b40baa54b
robertopiarulli-wq <roberto.piarulli@gmail.com>
2026-05-01 17:16:59 +0200

    Delete analysis directory

--- analysis/cooccurrence.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter
-import itertools
-
-def pair_analysis(ensemble):
-    pairs = Counter()
-    for s in ensemble:
-        pairs.update(itertools.combinations(s, 2))
-    return pairs
--- analysis/frequency.py
+++
@@ -1,7 +0,0 @@
-from collections import Counter
-
-def frequency_analysis(ensemble):
-    freq = Counter()
-    for s in ensemble:
-        freq.update(s)
-    return freq
